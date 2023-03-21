82f1397cbfb48fdff39e5836b20802512438c86f
Robert Popi <robertpopi@Roberts-MacBook-Air.local>
2023-03-21 10:36:20 -0500

    :white_check_mark: create user command test

--- test/commands/create-user.commands.spec.ts
+++ test/commands/create-user.commands.spec.ts
@@ -1,16 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserCommand, CreateUserCommandHandler } from 'src/application/commands/create-user.command';
+import { User } from 'src/domain/aggregates/user';
 import { UserInfrastructure } from 'src/infrastructure/user.infrastructure';
 
 
 let app: TestingModule;
 let createUserCommandHandler: any
+let userInfrastructure: UserInfrastructure;
+
 jest.mock('src/infrastructure/user.infrastructure', () => ({
     UserInfrastructure: jest.fn().mockImplementation(() => ({
         findByUserName: jest.fn().mockImplementation(() => null),
         save: jest.fn().mockImplementation(() => (
             {
-                properties: ()  => ({
+                properties: () => ({
                     userName: 'test',
                     password: 'test',
                     rol: 'test',
@@ -18,29 +21,49 @@ jest.mock('src/infrastructure/user.infrastructure', () => ({
                     createdAt: 'test',
                 }),
             }
-        ))  
-    }))  
+        ))
+    }))
 }));
 
 describe('CreateUserCommand', () => {
     beforeAll(async () => {
         app = await Test.createTestingModule({
-            providers: [CreateUserCommandHandler,UserInfrastructure]
+            providers: [CreateUserCommandHandler, UserInfrastructure]
         }).compile();
         createUserCommandHandler = app.get<CreateUserCommandHandler>(CreateUserCommandHandler);
+        userInfrastructure = app.get<UserInfrastructure>(UserInfrastructure);
+
     });
-    
+
 
     test('should create a user', async () => {
-        const command = new CreateUserCommand('test','test','test');
+
+        const date = new Date('2020-01-01');
+        global.Date = jest.fn(() => date) as any;
+        
+        const user = new User({
+            userName: 'test',
+            password: 'test',
+            rol: 'test',
+            id: 'test',
+        });
+    
+        const mockFindByUserName = jest
+            .spyOn(userInfrastructure, 'findByUserName')
+            .mockResolvedValueOnce(null);
+
+        const mockSave = jest
+            .spyOn(userInfrastructure, 'save')
+            .mockResolvedValueOnce(user)
+        const command = new CreateUserCommand('test', 'test', 'test');
         const result = await createUserCommandHandler.execute(command);
         expect(result).toEqual({
             id: 'test',
             userName: 'test',
             password: 'test',
             rol: 'test',
-            createdAt: 'test',
+            createdAt: new Date('2020-01-01'),
         })
     });
-    
+
 });
